714b1146be9ca49637c35ef6c4db30c19b9f98a5
Rajeshvarma2001 <rajeshvarman56@gmail.com>
2023-12-13 08:21:43 +0530

    hi

--- Rajesh06.py
+++ Rajesh06.py
@@ -1,4 +1,6 @@
-a=1
-b=2
-c=a+b
-print(c)
+global-message-list.transition {
+    -webkit-transition:  top 200ms linear;
+    -ms-transition:      top 200ms linear;
+    -moz-transition:     top 200ms linear;
+    -khtml-transition:   top 200ms linear;
+    -o-transition:       top 200ms linear;
